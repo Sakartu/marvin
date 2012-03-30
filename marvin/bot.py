939390836e5b472d5a98fa3105d12f9b1fac7119
Peter Wagenaar <sakartu@gmail.com>
2012-03-30 15:55:17 +0200

    adds new version of bot which can broadcast and handle github pollers

--- marvin/bot.py
+++ marvin/bot.py
@@ -1,16 +1,20 @@
 from twisted.words.protocols import irc
 from twisted.internet import protocol
 from message_handler import MessageHandler
+from github import IssuePoller
 
 class Marvin(irc.IRCClient):
 
     def _get_nickname(self):
-        return self.factory.nickname
+        return self.factory.conf.nickname
     nickname = property(_get_nickname)
 
     def signedOn(self):
-        self.join(self.factory.channel)
+        for chan in self.factory.conf.channels:
+            self.join(chan)
         print "Signed on as %s." % (self.nickname,)
+        for p in self.factory.pollers:
+            p.start()
 
     def joined(self, channel):
         print "Joined %s." % (channel,)
@@ -21,22 +25,34 @@ class Marvin(irc.IRCClient):
         else:
             self.handler.handle(user, channel, msg)
 
+    def broadcast(self, msg):
+        for chan in self.factory.conf.channels:
+            print chan, ':', msg
+            self.msg(chan, msg)
+
 class MarvinFactory(protocol.ClientFactory):
     protocol = Marvin
 
-    def __init__(self, channel, nickname):
-        self.channel = channel
-        self.nickname = nickname
+    def __init__(self, conf):
+        self.conf = conf
+        print "Starting github pollers"
 
     def buildProtocol(self, addr):
         bot = Marvin()
         bot.factory = self
-        bot.handler = MessageHandler(bot)
+        bot.handler = MessageHandler(self.conf, bot)
+        self.pollers = []
+        for (user, project) in self.conf.issues:
+            p = IssuePoller(self.conf, bot, user, project)
+            print 'Built poller for {user}/{project}'.format(user=user, 
+                    project=project)
+            self.pollers.append(p)
         return bot
 
     def clientConnectionLost(self, connector, reason):
-        print "Lost connection (%s), reconnecting." % (reason,)
-        connector.connect()
+        print "Lost connection!"
+        for p in self.pollers:
+            p.cancel()
 
     def clientConnectionFailed(self, connector, reason):
         print "Could not connect: %s" % (reason,)
