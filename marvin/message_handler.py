3c347e05c6bea87bcac2264325c17b06154ecc53
Peter Wagenaar <sakartu@gmail.com>
2012-04-02 21:51:34 +0200

    Adds handle_yesno and handle_choose

--- marvin/message_handler.py
+++ marvin/message_handler.py
@@ -1,6 +1,7 @@
 import re
 import random
 import constants
+import shlex
 
 class MessageHandler(object):
     def __init__(self, conf, bot):
@@ -11,18 +12,31 @@ class MessageHandler(object):
                 self.handle_question),
                 ('^.*{nick}.*$'.format(nick=self.bot.nickname),
                 self.handle_mention),
+                ('^.*{nick}: choose (.*)$'.format(nick=self.bot.nickname),
+                self.handle_choose),
                 ]
 
     def handle(self, user, channel, msg):
         for pattern, handler in self.handlers:
-            if re.compile(pattern).match(msg):
-                handler(user, channel, msg)
+            m = re.compile(pattern).match(msg)
+            if m:
+                handler(user, channel, msg, m)
                 return
 
-    def handle_question(self, user, channel, msg):
+    def handle_question(self, user, channel, msg, m):
         n = random.randint(0, len(constants.EIGHTBALL) - 1) 
         self.bot.msg(channel, constants.EIGHTBALL[n])
 
-    def handle_mention(self, user, channel, msg):
+    def handle_mention(self, user, channel, msg, m):
         n = random.randint(0, len(constants.ANSWERS) - 1) 
         self.bot.msg(channel, constants.ANSWERS[n])
+
+    def handle_choose(self, user, channel, msg, m):
+        if len(m.groups):
+            groups = shlex.split(m.group(1))
+            self.bot.msg(groups[random.randint(0, len(groups) - 1)])
+        else:
+            self.bot.msg("I can't really pick something from nothing, now can I?")
+
+    def handle_yesno(self, user, channel, msg, m):
+        self.bot.msg(random.choose(["Of course!", "Nah."]))
