fa42ac035584152f263677a526c324c527fce60a
Peter Wagenaar <sakartu@gmail.com>
2012-08-15 11:52:00 +0200

    Update to use a more generic configfile parser

--- marvin/config.py
+++ marvin/config.py
@@ -30,38 +30,49 @@ def parse_config(args):
 
     conf = Config()
 
-    def parse_value(conf, parser, args, section, value):
+    def parse_value(conf, parser, args, section, key):
+        '''
+        Parse a value from the config file
+        '''
         result = None
-        if value in dir(args) and getattr(args, value):
-            result = getattr(args, value)
+        if key in dir(args) and getattr(args, key):
+            result = getattr(args, key)
+        elif parser.has_option(section, key):
+            result = parser.get(section, key)
         else:
-            result = parser.get(section, value)
-        setattr(conf, value, result)
+            result = ''
+        setattr(conf, key, result)
         return conf
 
-    def parse_csvalue(conf, parser, args, section, value):
-        result = []
-        if value in dir(args) and getattr(args, value):
-            result = map(lambda x: tuple(x.split('/')),
-                    getattr(args, value).split(','))
-        else:
-            result = map(lambda x: tuple(x.split('/')),
-                    parser.get(section, value).split(','))
-        setattr(conf, value, result)
+    def parse_csvalue(conf, parser, args, section, key):
+        '''
+        Parse a comma separated value
+        '''
+        conf = parse_value(conf, parser, args, section, key)
+        setattr(conf, key, tuple(getattr(conf, key).split(',')))
+        return conf
+
+    def parse_csssvalue(conf, parser, args, section, key):
+        '''
+        Parse a comma, then slash separated value
+        '''
+        conf = parse_csvalue(conf, parser, args, section, key)
+        vs = getattr(conf, key)
+        setattr(conf, key, tuple(tuple(x.split('/')) for x in vs))
         return conf
 
     # Parse configuration options
-    for v in ('server', 'port', 'nickname', 'realname'):
-        conf = parse_value(conf, parser, args, 'general', v)
+    for k in ('server', 'port', 'nickname', 'realname'):
+        conf = parse_value(conf, parser, args, 'general', k)
 
-    for v in ('channels'):
-        conf = parse_csvalue(conf, parser, args, 'general', v)
+    for k in ('channels',):
+        conf = parse_csvalue(conf, parser, args, 'general', k)
 
-    for v in ('polltime'):
-        conf = parse_value(conf, parser, args, 'github', v)
+    for k in ('polltime',):
+        conf = parse_value(conf, parser, args, 'github', k)
 
-    for v in ('commits', 'issues'):
-        conf = parse_csvalue(conf, parser, args, 'github', v)
+    for k in ('commits', 'issues'):
+        conf = parse_csssvalue(conf, parser, args, 'github', k)
 
     normalised = []
     for c in conf.channels:
@@ -81,9 +92,12 @@ class Config(object):
         self.commits = []
 
     def __repr__(self):
-        result = self.nickname + '@' + self.server + ':' + str(self.port)
-        + '\n'
+        result = (self.nickname + '@' + self.server + ':' + str(self.port)
+                + '\n')
         result += 'Channels: ' + pprint.pformat(self.channels)
         result += '\nIssues: ' + pprint.pformat(self.issues)
         result += '\nCommits: ' + pprint.pformat(self.commits)
         return result
+
+    def __str__(self):
+        return repr(self)
