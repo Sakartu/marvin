33aa44f9f0bc61a477b05defdd94d7467a4e835f
Peter Wagenaar <sakartu@gmail.com>
2012-08-24 16:33:41 +0200

    Use curses instead of pure cmd

--- marvin/tui.py
+++ marvin/tui.py
@@ -1,11 +1,17 @@
 import util
 import cmd
 import sys
+import curses
+import curses.textpad
+import datetime
 
 
 class MarvinTUI(cmd.Cmd):
     def __init__(self, conf):
         cmd.Cmd.__init__(self)
+
+        self.setup_screens()
+        self.reroute_stdio()
 
         self.conf = conf
         self.bot = None
@@ -13,26 +19,13 @@ class MarvinTUI(cmd.Cmd):
         self.prompt = u'marvin$ '
         self.intro = u'Welcome to marvin, the depressed IRC bot'
         self.doc_header = u'Commands (press help <command> to get help):'
+        self.msg(u'Connecting to server...')
 
     def cmdloop(self):
         while self.running:
-            try:
-                cmd.Cmd.cmdloop(self)
-            except UserCancelled:
-                self.intro = u''
-                continue
-
-    def preloop(self):
-        self.do_help("")
-
-    def postloop(self):
-        print
-
-    def precmd(self, line):
-        if line == "EOF":
-            raise UserCancelled
-        else:
-            return line
+            self.cmdwin.addstr(self.prompt)
+            text = self.cmdwin.getstr()
+            self.onecmd(text)
 
     def do_join(self, line=None):
         if not line:
@@ -42,14 +35,14 @@ class MarvinTUI(cmd.Cmd):
             self.bot.connection.join(util.get_channel(c))
 
     def help_join(self):
-        print u'Join one or more channels given as arguments.'
-        print u'usage: join [channel]...'
+        self.query(u'Join one or more channels given as arguments.')
+        self.query(u'usage: join [channel]...')
 
     def do_reload(self, line=None):
         self.bot.handler.load_all()
 
     def help_reload(self):
-        print u'Reload all the resources (quotes and stuff)'
+        self.query(u'Reload all the resources (quotes and stuff)')
 
     def do_say(self, line=None):
         if not line:
@@ -59,8 +52,8 @@ class MarvinTUI(cmd.Cmd):
         self.bot.connection.privmsg(target, msg)
 
     def help_say(self):
-        print u'Say something to a target (channel or user)'
-        print u'usage: say [target] <msg>'
+        self.query(u'Say something to a target (channel or user)')
+        self.query(u'usage: say [target] <msg>')
 
     def do_broadcast(self, line=None):
         pass
@@ -69,43 +62,81 @@ class MarvinTUI(cmd.Cmd):
         pass
 
     def do_status(self, line=None):
-        print u'The bot is known as {nick}({real})'.format(
-                nick=self.conf.nickname, real=self.conf.realname)
-        print u'{nick} is joined in:'.format(nick=self.conf.nickname)
+        self.query(u'The bot is known as {nick}({real})'.format(
+                nick=self.conf.nickname, real=self.conf.realname))
+        self.query(u'{nick} is joined in:'.format(nick=self.conf.nickname))
         for i in self.conf.channels:
-            print i
+            self.query(i)
 
     def help_status(self, line=None):
-        print u'Print some status information about the bot, including which '
-        'channels it\'s joined in'
+        self.query(u'Print some status information about the bot, including '
+                'which channels it\'s joined in')
 
     def do_quit(self, line=None):
         '''
         Quits the program
         '''
-        print u'\nDisconnecting and exitting...'
+        self.query(u'\nDisconnecting and exitting...')
         if self.bot:
             for p in self.bot.pollers:
                 p.cancel()
             self.bot.die()
+        curses.endwin()
         sys.exit(0)
 
     def help_quit(self):
-        print u'Quit the application'
+        self.query(u'Quit the application')
 
     def help_help(self):
-        print u'Get help about a topic'
+        self.query(u'Get help about a topic')
 
+    def setup_screens(self):
+        self.screen = curses.initscr()
+        self.maxy, self.maxx = self.screen.getmaxyx()
 
-class TUIException(Exception):
-    '''
-    A TUI exception
-    '''
-    pass
+        # Left screen
+        self.left = self.screen.subwin(self.maxy, self.maxx / 2, 0, 0)
+        self.left.box()
+        self.left.addstr(0, 3, 'Commands')
+        self.left.leaveok(True)
+        self.left.idlok(True)
+        self.left.scrollok(True)
+        innery, innerx = tuple(x + 1 for x in self.left.getbegyx())
+        rows, cols = self.left.getmaxyx()
+        innerrows = innery + rows - 3
+        innercols = innerx + cols - 3
+        self.cmdwin = self.left.subwin(innerrows, innercols, innery, innerx)
 
+        # Right screen
+        self.right = self.screen.subwin(self.maxy, self.maxx / 2, 0,
+                self.maxx / 2)
+        self.right.box()
+        self.right.addstr(0, 3, 'Log')
+        self.padmaxy, self.padmaxx = self.right.getmaxyx()
+        self.rightpad = curses.newpad(1000, self.padmaxx)
+        self.rightscroll = 0
+        self.screen.refresh()
+        self.msg('Setup done!')
 
-class UserCancelled(TUIException):
-    '''
-    This class is raised when the user presses ^D in the TUI
-    '''
-    pass
+    def reroute_stdio(self):
+        query = self.query
+
+        class NewOut(object):
+            def write(self, m):
+                query(m)
+
+        self.stdout = NewOut()
+
+    def query(self, m):
+        self.cmdwin.addstr(m if m.endswith('\n') else m + '\n')
+
+    def msg(self, m):
+        m = datetime.datetime.now().strftime('[%x %X]: ') + unicode(m)
+        self.rightpad.addstr(m if m.endswith('\n') else m + '\n')
+        self.rightscroll += 1
+        offset = self.rightscroll - self.padmaxy + 2
+        self.rightpad.refresh(offset, 0, 1, self.padmaxx + 1,
+                self.padmaxy - 2, self.maxx - 2)
+
+    def write(self, m):
+        self.msg(m)
